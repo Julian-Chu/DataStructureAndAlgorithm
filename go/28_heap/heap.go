e7bc37c9d8908e50e4af7f27cccb508c15b72278
Julian Chu <yulang.chu@gmail.com>
2020-09-06 21:52:47 +0200

    heap

--- go/28_heap/heap.go
+++ go/28_heap/heap.go
@@ -1,1 +1,70 @@
 package _8_heap
+
+type Heap struct{
+	a []int
+	n int
+	count int
+}
+
+//init heap
+func NewHeap(capacity int) *Heap{
+	heap:= &Heap{}
+	heap.n = capacity
+	heap.a = make([]int, capacity)
+	heap.count = 0
+	return heap
+}
+
+//top-max heap -> heapify from down to up
+func (heap Heap) insert(data int) {
+	// defensive
+	if heap.count == heap.n{
+		return
+	}
+
+	heap.count++
+	heap.a[heap.count] = data
+
+	//compare with parent node
+	i:= heap.count
+	parent:=i/2
+	for parent>0 &&heap.a[parent] < heap.a[i]{
+		heap.a[parent], heap.a[i] = heap.a[i], heap.a[parent]
+		i = parent
+		parent = i/2
+	}
+}
+
+//heapify from up to down
+func (heap *Heap) removeMax()  {
+   //defensive
+	if heap.count==0{
+		return
+	}
+
+	//swap max and last
+	heap.a[1], heap.a[heap.count] = heap.a[heap.count], heap.a[1]
+	heap.count--
+	heapifyUpToDown(heap.a, heap.count)
+}
+
+func heapifyUpToDown(a []int, count int) {
+	for i := 1; i <= count/2; {
+		maxIndex:=i
+		if a[i]<a[i*2]{
+			maxIndex = i*2
+		}
+
+		if i*2+1 <=count && a[maxIndex] < a[i*2+1]{
+			maxIndex = i*2+1
+		}
+
+		if maxIndex ==i{
+			break
+		}
+
+		a[i], a[maxIndex] = a[maxIndex],a[i]
+		i = maxIndex
+
+	}
+}
--- go/28_heap/heap_sort.go
+++ go/28_heap/heap_sort.go
@@ -1,1 +1,28 @@
 package _8_heap
+
+func buildHeapd(a []int, n int) {
+	//heapify from the last parent node
+	for i := n / 2; i >= i; i-- {
+		heapifyUp2Down(a, i, n)
+	}
+}
+
+//heapify from up to down, node index = top
+func heapifyUp2Down(a []int, top int, count int) {
+	for i := top; i <= count/2; {
+		maxIndex := i
+		if a[i] < a[i*2] {
+			maxIndex = i * 2
+		}
+
+		if i*2+1 <= count && a[maxIndex] < a[i*2+1] {
+			maxIndex = i*2 + 1
+		}
+
+		if maxIndex == i {
+			break
+		}
+		a[i], a[maxIndex] = a[maxIndex],a[i]
+		i = maxIndex
+	}
+}
